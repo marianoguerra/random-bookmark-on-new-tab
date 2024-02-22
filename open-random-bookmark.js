179db626cccb1fa9a3c12d944fbe448666e60513
Mariano Guerra <mariano@marianoguerra.org>
2024-02-22 16:21:55 +0100

    wait less, try to avoid navigating if tab changed, use more async, time bookmark collection

--- open-random-bookmark.js
+++ open-random-bookmark.js
@@ -1,27 +1,41 @@
-function openRandomBookmark(tab) {
+async function openRandomBookmark(tab) {
   // Fetch all bookmarks
-  browser.bookmarks.getTree().then((bookmarkItems) => {
-    let bookmarks = [];
-    function findBookmarks(item) {
-      if (item.url) bookmarks.push(item.url);
-      if (item.children) {
-        item.children.forEach((child) => findBookmarks(child));
-      }
-    }
-    bookmarkItems.forEach(findBookmarks);
+  const bookmarkItems = await browser.bookmarks.getTree(),
+    t1 = Date.now(),
+    bookmarks = [];
 
-    // Navigate the current tab to a random bookmark if there are any bookmarks
-    if (bookmarks.length > 0) {
-      const randomBookmark =
-        bookmarks[Math.floor(Math.random() * bookmarks.length)];
-      browser.tabs.update(tab.id, { url: randomBookmark });
+  function findBookmarks(item) {
+    if (item.url) bookmarks.push(item.url);
+    if (item.children) {
+      item.children.forEach((child) => findBookmarks(child));
     }
-  });
+  }
+  bookmarkItems.forEach(findBookmarks);
+
+  console.log("got bookmarks in", Date.now() - t1, "ms");
+
+  // Navigate the current tab to a random bookmark if there are any bookmarks
+  if (bookmarks.length > 0) {
+    const bookmarkIndex = Math.floor(Math.random() * bookmarks.length),
+      randomBookmark = bookmarks[bookmarkIndex];
+
+    const newTab = await browser.tabs.get(tab.id);
+    if (isEmptyTabAndComplete(newTab)) {
+      console.log("loading bookmark", newTab, randomBookmark);
+      browser.tabs.update(tab.id, { url: randomBookmark });
+    } else {
+      console.log("tab changed before loading bookmark", newTab);
+    }
+  }
 }
 
 function isEmptyTab(tab) {
   return tab.url === "about:newtab" || tab.url === "about:blank" ||
     tab.url === "about:home";
+}
+
+function isEmptyTabAndComplete(tab) {
+  return tab.status === "complete" && isEmptyTab(tab);
 }
 
 browser.tabs.onCreated.addListener((tab) => {
@@ -37,12 +51,12 @@ browser.tabs.onCreated.addListener((tab) => {
     // we just wait a while and if still empty
     setTimeout(async () => {
       const newTab = await browser.tabs.get(tab.id);
-      if (newTab.status === "complete" && isEmptyTab(newTab)) {
+      if (isEmptyTabAndComplete(newTab)) {
         openRandomBookmark(newTab);
       } else {
         console.log("after: not empty", newTab);
       }
-    }, 500);
+    }, 200);
   } else {
     console.log("onCreated: not empty", tab);
   }
